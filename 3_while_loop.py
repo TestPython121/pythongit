b14aa524019bf14b7dde62fcef24ade6c4cca202
pythongit <kodeanji6@gmail.com>
2021-01-25 16:39:21 +0530

    add file

---
+++ 3_while_loop.py
@@ -0,0 +1,17 @@
+'''
+1. While loop Iterate based on condition
+'''
+n = 6
+#while n > 3:
+  #  n -= 2
+   # print(n)
+
+#while n:
+   # print(n)
+   #  n -= 2
+
+sal = 500
+while sal <= 300:
+   print(sal)
+   sal -= 50
+print('exit loop:', sal)
